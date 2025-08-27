cbe34626e43bf440e419d495d120dc2722a9b87c
knu9910 <knu9910@naver.com>
2025-08-27 14:26:06 +0900

    feat: zod 사용해보기

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -1,12 +1,22 @@
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+import z from 'zod';
 
 const app = new Hono().basePath('/api');
 
-app.get('/hello', (c) => {
+const route = app.get('/hello', zValidator('query', z.object({ name: z.string() })), (c) => {
+  const { name } = c.req.valid('query');
   return c.json({
-    message: 'Hello from Hono!',
+    message: `Hello ${name}`,
   });
 });
 
 export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+export const PATCH = handle(app);
+export const OPTIONS = handle(app);
+
+export type AppType = typeof route;
